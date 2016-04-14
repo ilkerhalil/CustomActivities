0e521a693e776d4f6c7f0a07061a6d4494227654
Ilker Halil Turer <ilkerhalil@gmail.com>
2016-04-14 17:28:15 +0300

    Add project files.

---
+++ CustomActivities.Tests/GetFeedTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Activities;
+using System.Linq;
+using System.ServiceModel.Syndication;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CustomActivities.Tests
+{
+    [TestClass]
+    public class GetFeedTests
+    {
+        [TestMethod]
+        public void Test_Feed_Returns_Correct_Amount_Of_Results()
+        {
+            var act = new GetFeed
+            {
+                FeedUrl= "http://www.ntv.com.tr/gundem.rss"
+            };
+            var feed = WorkflowInvoker.Invoke<SyndicationFeed>(act);
+            Assert.IsNotNull(feed,"No feed returned");
+            Assert.AreEqual(49,feed.Items.Count(),"Incorrect number of items");
+        }
+    }
+}
---
+++ CustomActivities/GetFeed.cs
@@ -0,0 +1,19 @@
+﻿using System.Activities;
+using System.ServiceModel.Syndication;
+using System.Xml;
+
+namespace CustomActivities
+{
+    public class GetFeed : CodeActivity<SyndicationFeed>
+    {
+        public InArgument<string> FeedUrl { get; set; }
+
+        public OutArgument<SyndicationFeed> FeedResult { get; set; }
+
+        protected override SyndicationFeed Execute(CodeActivityContext context)
+        {
+            var feed = SyndicationFeed.Load(XmlReader.Create(FeedUrl.Get(context)));
+            return feed;
+        }
+    }
+}
